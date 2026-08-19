ccb8f2612031786f04ae6e912d1dd5ff2c1d788e
Alex <razor3726@gmail.com>
2026-08-19 19:35:13 +0300

    Delete mainwindow directory

--- mainwindow/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_mainwindow_FILE D:/Qt/Qt_cpp/mainwindow/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/mainwindow.exe)
-set(__QT_DEPLOY_TARGET_mainwindow_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_mainwindow_RUNTIME_DLLS D:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
